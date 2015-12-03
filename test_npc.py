1833039b1ad53b9d1edb62b48164cf52ae566f4e
huanghansong <hansong.huang@yahoo.com>
2015-12-02 19:35:37 -0600

    test subclasses of npc too

--- test_npc.py
+++ test_npc.py
@@ -2,17 +2,19 @@ from buffalo import utils
 from npc import NPC
 from enemy import Enemy
 from friendly import Friendly
+from trader import Trader
 
 utils.init()
 isWorking = False
-A = NPC.__init__(Enemy, 3, 10, 1)
-B = NPC.__init__(Friendly, 2, 25, 3)
-npcs = [A, B]
+enemy = Enemy(name="monster", fPos=(600.0,600.0))
+friendly = Friendly(name="villager", fPos=(650.0,650.0))
+trader = Trader(name="merchant", fPos=(800.0,800.0))
+npcs = [enemy, friendly, trader]
 
 for npc in npcs:
     NPC.load_sprites()
-    A.move("d")
-    B.move("r")
+    NPC.move_X(4)
+    NPC.move_Y(3)
     NPC.detectCollision(npcs)
     NPC.blit_sprite()
     
