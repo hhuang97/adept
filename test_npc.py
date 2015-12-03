df386c1b479fa9105cae9852d7bb6e2cc7016335
huanghansong <hansong.huang@yahoo.com>
2015-12-02 19:30:06 -0600

    try again

--- test_npc.py
+++ test_npc.py
@@ -1,10 +1,12 @@
 from buffalo import utils
 from npc import NPC
+from enemy import Enemy
+from friendly import Friendly
 
 utils.init()
 isWorking = False
-A = NPC.__init__(NPC, 3, 10, 1)
-B = NPC.__init__(NPC, 2, 25, 3)
+A = NPC.__init__(Enemy, 3, 10, 1)
+B = NPC.__init__(Friendly, 2, 25, 3)
 npcs = [A, B]
 
 for npc in npcs:
